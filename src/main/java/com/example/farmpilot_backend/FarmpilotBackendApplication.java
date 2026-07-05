f16b4e6f6b6d5925b76f61ee72d34a8c3c6f08da
alisha2112 <alinatomchak5@gmail.com>
2026-07-05 15:31:09 +0300

    Initial commit

---
+++ src/main/java/com/example/farmpilot_backend/FarmpilotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.farmpilot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmpilotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmpilotBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/farmpilot_backend/FarmpilotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmpilot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmpilotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
