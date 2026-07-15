fb3d3fceaddc64b21724ab5254b8f6dc362cbd1a
alisha2112 <alinatomchak5@gmail.com>
2026-07-15 14:36:38 +0300

    feat(websocket): integrate SimpMessagingTemplate in AlertConsumer to broadcast RabbitMQ alerts to frontend

--- src/main/java/com/example/farmpilot_backend/consumer/AlertConsumer.java
+++ src/main/java/com/example/farmpilot_backend/consumer/AlertConsumer.java
@@ -5,6 +5,7 @@ import com.example.farmpilot_backend.service.NotificationLogService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,9 +13,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AlertConsumer {
     private final NotificationLogService notificationLogService;
+    private final SimpMessagingTemplate messagingTemplate;
     @RabbitListener(queues = RabbitMQConfig.QUEUE_ALERTS)
     public void receiveAlert(String message) {
         log.info("[RABBITMQ LISTENER] Caught new alert: {}", message);
         notificationLogService.saveSystemAlert(message);
+        messagingTemplate.convertAndSend("/topic/alerts", message);
+        log.info("[WEBSOCKET] Сповіщення успішно трансльовано у канал /topic/alerts");
     }
 }
